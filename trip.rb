28d8bb790764ad3009161b918e4ccaea5bafeb7a
Bridget Kienzle <bridgetkien@gmail.com>
2018-07-23 20:10:16 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -18,3 +18,5 @@ eat = gets.chomp.capitalize
 
 puts "How long would you like to stay?"
 duration = gets.chomp.capitalize + "nights"
+
+"You are going to stay at #{stay}. You will eat at #{eat}. You will stay for #{duration}."
